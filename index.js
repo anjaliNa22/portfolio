f17d03240c2549bdaa43152686f0c53c397844a3
Anjali <it20141356@my.sliit.lk>
2024-01-28 10:57:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+function redirectToURL() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://www.figma.com/file/LFqjgHhN1ojZA2A8dy6A6f/Untitled?node-id=0%3A1';
+  }
+
+
+  function redirectToURLglobal() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://www.figma.com/file/J9QiZOhvumbEQvlBjziLsF/global-edulink-redisign?type=design&node-id=125%3A7140&t=knZNWrojeukJqdNY-1';
+  }
+  
+  function evcharge() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://www.figma.com/file/YaHKySJGwQqGnid4Xf0fTB/Untitled?node-id=0%3A1';
+  }
+
+  function todo() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://bnaauction.lk/';
+  }
+
+  function store() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://www.linkedin.com/posts/anjali-navodya-6286a6205_project-javascript-sliit-activity-6866195574924840960-AV21?utm_source=linkedin_share&utm_medium=member_desktop_web';
+  }
+  function food() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://www.linkedin.com/posts/anjali-navodya-6286a6205_scorpion-andriodapp-design-activity-6850368206037692416-8XW8?utm_source=linkedin_share&utm_medium=member_desktop_web';
+  }
+  function mart() {
+    // Replace 'https://www.example.com' with the URL you want to redirect to
+    window.location.href = 'https://github.com/PasinduPramodya/ITPM_Kwikmat.git';
+  }
+  
+  
+
+  
